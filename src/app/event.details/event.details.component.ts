08beeb5e4724066cd3e6b36fcfdf5a1d6a77570f
Carlos Martinez <itscarlosisaac@gmail.com>
2017-09-22 00:47:22 -0400

    Added router to single events via ID.

--- src/app/event.details/event.details.component.ts
+++ src/app/event.details/event.details.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core'
 import { EventsService } from '../events/shared/events.service'
+import { ActivatedRoute } from '@angular/router'
+
 @Component({
   templateUrl:'./event.details.component.html'
 })
 
 export class EventDetailsComponent {
   event:any;
-  constructor( private eventService: EventsService ){}
+  constructor(
+    private eventService: EventsService,
+    private route: ActivatedRoute
+   ){}
 
   ngOnInit(){
-    this.event = this.eventService.getEvent(1)
+    let id = parseInt( this.route.snapshot.params['id'] );
+    this.event = this.eventService.getEvent(id)
   }
 }
